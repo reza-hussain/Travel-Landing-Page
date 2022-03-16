beddfa7f983eb8cdd125ca73e4376d1ec2a2f9da
Reza Hussain <alirezaa08@gmail.com>
2022-03-16 06:05:23 +0530

    Changes gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-module.exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     if (stage === "build-html") {
       actions.setWebpackConfig({
         module: {
