5d784b34d3edd0335295c318f811127610b1f8db
Reza Hussain <alirezaa08@gmail.com>
2022-03-16 09:34:51 +0530

    Styling Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     <>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
+    <link href="http://fonts.cdnfonts.com/css/euclid-circular-a" rel="stylesheet"></link>
     <div className='main'>
     <Header />
     <Jumbotron />
